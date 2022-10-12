2fb2574ec4eab300498ef502823730a4c79cae72
Luis Gonzalez <gg0768@wayne.edu>
2022-10-12 17:59:20 -0400

    created initial about me template

---
+++ src/Components/Aboutme.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Box, Typography } from '@mui/material'
+
+
+const Aboutme = () => {
+  return (
+    <Box component="span">
+        <text>Some Image Goes Here</text>
+        <Typography>About Me Section</Typography>
+    </Box>
+  )
+}
+
+export default Aboutme
