3a1a0dd38767e6d67dfd6ade53d6c9f304e17980
Luis Gonzalez <gg0768@wayne.edu>
2022-10-12 18:01:30 -0400

    added template components to app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import logo from './logo.svg';
+import Header from './Components/Header';
+import Aboutme from './Components/Aboutme';
 import './App.css';
+import Experience from './Components/Experience';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header />
+        <Aboutme />
+        <Experience />
+        <Footer />
       </header>
     </div>
   );
