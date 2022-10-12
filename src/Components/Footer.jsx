f4a8e7b4b52b7489fce3ee3f232c0a43b9a5b52e
Luis Gonzalez <gg0768@wayne.edu>
2022-10-12 17:58:43 -0400

    created initial footer template

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Box, Typography } from '@mui/material'
+
+
+const Footer = () => {
+  return (
+    <Box>
+        <Typography>Footer links and such</Typography>
+    </Box>
+  )
+}
+
+export default Footer
