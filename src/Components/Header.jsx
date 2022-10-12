791a181fe293213a271e50963d09e31a6894d150
Luis Gonzalez <gg0768@wayne.edu>
2022-10-12 17:58:26 -0400

    created initial header template

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from '@mui/material'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <Box>
+        <Typography>This is the header</Typography>
+    </Box>
+  )
+}
+
+export default Header
